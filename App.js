130b494dbf57b3922053637c675e862f0c8186fe
atchuuu <112748711+atchuuu@users.noreply.github.com>
2025-01-27 10:32:29 +0530

    Timer in React
    used usestates in react to create a work and break timer

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+//import News from './Components/News';
+//import Use from './Components/Counter';
+//import FormControlled from './Components/FormControlled';
+//import RouteDom from './Components/Routes/RouteDom';
+//import Comp1 from './Components/CSS/Comp1';
+import ProjectTimer from './Components/ProjectTimer';
+//import Timer from './Components/Timer';
+function App() {
+  return (
+    <div>
+      <ProjectTimer />
+    </div>
+  );
+}
+
+export default App;
